5abd92fc1bd6b145ca4a73ca43e027100812f8d1
Calum Cuthill <calum.cuthill@gmail.com>
2023-10-19 09:56:51 +0100

    binpack command working!

---
+++ make_binpack.sh
@@ -0,0 +1,1 @@
+~/git/Notecard-DFU/note-cli_1.2.1_macOS_arm64/notecard -binpack esp32 0x0:.pio/build/m5stack-cores3/bootloader.bin 0x8000:.pio/build/m5stack-cores3/partitions.bin 0x10000:.pio/build/m5stack-cores3/firmware.bin
