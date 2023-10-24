9eb62dd9a1f90f4ee56bc9f50b1981f23e5a69b2
Calum Cuthill <calum.cuthill@gmail.com>
2023-10-24 11:36:45 +0100

    gui and rtc helpers, simplifiction etc

--- src/main.cpp
+++ src/main.cpp
@@ -13,23 +13,11 @@
 
 #include "notecard_manager.h"
 #include "rtos_tasks.h"
+#include "gui_helpers.h" 
+#include "rtc_helpers.h" 
 
 // #include "App.h"
 #include <ui.h>
-
-#define PIN_SDA_I2C_SYS 12
-#define PIN_SCL_I2C_SYS 11
-#define PIN_SDA_I2C_EXT 2
-#define PIN_SCL_I2C_EXT 1
-
-#define PIN_RX_RS485 18
-#define PIN_TX_RS485 17
-#define PIN_DE_RS485 5
-
-#define PIN_RX0 44
-#define PIN_TX0 43
-
-// #define PIN_PULSE_COUNT 8 // Put in rtos_tasks.cpp for now, should be moved back
 
 #define YOTTA_MODULE_ID 0
 #define NOTECARD_FETCH_INTERVAL_MS (10 * 1000)
@@ -62,42 +50,40 @@ QuickPID compressorPID(
     &db_vars.dhw_setpoint               //Setpoint
 );   
 
-char buffer[64]; // is this used?
-
 void setup() {
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-        ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
 
+    //Default is all 3 options disabled, AKA POWER_MODE_USB_IN_BUS_IN
     M5.In_I2C.bitOff(AW9523_ADDR, 0x03, 0b10000000, 100000L);  // BOOST_EN = 0
-    // M5.In_I2C.bitOff(AW9523_ADDR, 0x02, 0b00000010, 100000L);  // BUS_OUT_EN = 0
-    M5.In_I2C.bitOn(AW9523_ADDR, 0x02, 0b00000010, 100000L);  // BUS_OUT_EN = 1
+    // M5.In_I2C.bitOn(AW9523_ADDR, 0x03, 0b10000000, 100000L);  // BOOST_EN = 1
 
-    USBSerial.begin(15200);
+    M5.In_I2C.bitOff(AW9523_ADDR, 0x02, 0b00000010, 100000L);  // BUS_OUT_EN = 0
+    // M5.In_I2C.bitOn(AW9523_ADDR, 0x02, 0b00000010, 100000L);  // BUS_OUT_EN = 1
+
+    M5.In_I2C.bitOff(AW9523_ADDR, 0x02, 0b00100000, 100000L);  // USB_OTG_EN = 0
+    // M5.In_I2C.bitOn(AW9523_ADDR, 0x02, 0b00100000, 100000L);  // USB_OTG_EN = 1
+
+    USBSerial.begin();
+    while (!USBSerial) {
+    ; // wait for serial port to connect. Needed for native USB
+    }
     M5.begin();
 
     Wire.begin(PIN_SDA_I2C_EXT, PIN_SCL_I2C_EXT, 400000);  //Init I2C_EXT
     notecardManager.begin();
 
     USBSerial.printf("M5CoreS3 User Demo, Version: %s\r\n", DEMO_VERSION);
-
-    // // BM8563 Init (clear INT)
-    // M5.In_I2C.writeRegister8(0x51, 0x00, 0x00, 100000L);
-    // M5.In_I2C.writeRegister8(0x51, 0x01, 0x00, 100000L);
-    // M5.In_I2C.writeRegister8(0x51, 0x0D, 0x00, 100000L);
     M5.Display.setBrightness(60);
 
     lv_init();
     m5gfx_lvgl_init();
     ui_init();
+    setup_gui_timers();
     setup_rtos_tasks();
+
+
 
 }
 
 void loop() {
     // Nothing here. All the work is done in the RTOS tasks
-
 }
